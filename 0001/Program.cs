0179d5b2c251d833a9b7f74b01901464c5f35ad3
Julia.S <der.lavender@gmail.com>
2022-08-17 15:45:57 +0300

    0001

---
+++ 0001/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход 
+//пятизначное число и проверяет, является ли оно палиндромом.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int num = Prompt("Введите пятизначное число ");
+if (num > 100000 || num < 10000)
+{
+    System.Console.WriteLine("error enter");
+    
+}
+else if (num % 10000 / 1000 == num % 100 / 10 && num % 100000 / 10000 == num % 10)
+{
+    System.Console.WriteLine("Polyndrome");
+    
+    }
+else
+{
+    System.Console.WriteLine("not polyndrome");
+}
+
